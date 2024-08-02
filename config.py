801b307ca890bc2724ffaa9da138f847f97babb1
Eren Yeager <154471897+SquirtleRivals09@users.noreply.github.com>
2024-08-02 19:55:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,21 @@
 import os
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", " 7381512423:AAGgaCvl-Wu5EEaf9eOyDfqPYcOuz_2e9GA
+")
+API_ID = int(os.environ.get("API_ID", "29422639"))
+API_HASH = os.environ.get("API_HASH", "e21bccfd64a01c5762ce81c77379dc7f")
+OWNER = int(os.environ.get("OWNER", "5106602523"))
+BOT_USERNAME =  os.environ.get('BOT_USERNAME', "YorForger_Probot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1001739122591")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002226603933"))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://Aaru:AaruAaru@cluster0.qthztcs.mongodb.net/?retryWrites=")
+DB_NAME = os.environ.get("DB_NAME", "xyz")
 
 STRING = os.environ.get("STRING", "")
-BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
+BOT_PIC = os.environ.get("BOT_PIC", "https://telegra.ph//file/9f69ff30c0c49ad9d941c.jpg")
 
 
 
